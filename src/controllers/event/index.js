fae87736d291939aeb5d7583753eb946afdcfb27
SamuelPickering <samuelpickering21@gmail.com>
2024-01-28 12:02:34 -0500

    All controllers working

---
+++ src/controllers/event/index.js
@@ -0,0 +1,21 @@
+const getEventsOfUser = require("./getEventsOfUser")
+const getSignedUpEvents = require("./getSignedUpEvents")
+const getRecentEvents = require("./getRecentEvents")
+const getUsersInEvent = require("./getUsersInEvent")
+const postEvent = require("./postEvent")
+const joinEvent = require("./joinEvent")
+const leaveEvent = require("./leaveEvent")
+const addTags = require("./addTags")
+
+module.exports = {
+    getEventsOfUser,
+    getSignedUpEvents,
+    getRecentEvents,
+    getUsersInEvent,
+    postEvent,
+    joinEvent,
+    leaveEvent,
+    addTags,
+    
+
+}
